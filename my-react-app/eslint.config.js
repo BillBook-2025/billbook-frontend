001cd08be12d61d6a36e8fe651fe14e27760ae71
jungmin Han <jmni.hhh@gmail.com>
2025-05-27 17:54:44 +0900

    chore: ESLint+Prettier 연동

--- my-react-app/eslint.config.js
+++ my-react-app/eslint.config.js
@@ -1,33 +1,21 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
 
-export default [
-  { ignores: ['dist'] },
+export default defineConfig([
   {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js, prettier: pluginPrettier },
+    extends: ["js/recommended", "plugin:prettier/recommended"],
     rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "prettier/prettier": "error",
+    },
+    languageOptions: {
+      globals: globals.browser,
     },
   },
-]
+  pluginReact.configs.flat.recommended,
+]);
