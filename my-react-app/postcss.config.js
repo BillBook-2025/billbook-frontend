44fe81fd73954002062d3ef833c0f665c2062462
jungmin Han <jmni.hhh@gmail.com>
2025-05-27 18:33:40 +0900

    chore: 테일윈드 설정 오오류 작업

--- my-react-app/postcss.config.js
+++ my-react-app/postcss.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
-}
+// postcss.config.js (ES 모듈)
+
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [tailwindcss, autoprefixer],
+};
