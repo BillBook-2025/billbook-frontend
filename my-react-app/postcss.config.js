13e233fadf39441fdb4a2d9adbab5cce748a5548
jungmin Han <jmni.hhh@gmail.com>
2025-09-29 23:59:00 +0900

    fix: 플러그인 업데이트

--- my-react-app/postcss.config.js
+++ my-react-app/postcss.config.js
@@ -1,8 +1,8 @@
 // postcss.config.js (ES 모듈)
 
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-  plugins: [tailwindcss, autoprefixer],
-};
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+}
